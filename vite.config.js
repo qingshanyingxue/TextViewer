392920e395b59c4aab51d71669419cc8c7a19995
wangguangjin <1139039196@qq.com>
2026-04-28 09:10:48 +0800

    自动创建桌面快捷方式

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,27 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 2,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue:      ['vue'],
+          markdown: ['markdown-it', 'markdown-it-anchor'],
+          hljs:     ['highlight.js'],
+        },
+        chunkFileNames:  'assets/[name]-[hash].js',
+        assetFileNames:  'assets/[name]-[hash][extname]',
+        entryFileNames:  'assets/[name]-[hash].js',
+      },
+    },
+    chunkSizeWarningLimit: 1000,
   },
   server: {
     port: 5173,
